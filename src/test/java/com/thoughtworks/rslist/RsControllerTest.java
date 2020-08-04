5f7e1c21da90b019bd28d8fff59da1b73a822515
twodogbeta <qijinhaoup@163.com>
2020-08-05 00:04:09 +0800

    finish getOneRs

---
+++ src/test/java/com/thoughtworks/rslist/RsControllerTest.java
@@ -0,0 +1,39 @@
+package com.thoughtworks.rslist;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Test
+    void shouldGetRsList() throws Exception{
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"))
+                .andExpect(status().isOk());
+    }
+    @Test
+    void shouldGetOneEvent() throws Exception {
+        mockMvc.perform(get("/rs/1"))
+                .andExpect(content().string("第一条事件"))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/2"))
+                .andExpect(content().string("第二条事件"))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/3"))
+                .andExpect(content().string("第三条事件"))
+                .andExpect(status().isOk());
+
+    }
+}
