4bbacde97e65c509644e9ec75d4c82aa75d1af30
twodogbeta <qijinhaoup@163.com>
2020-08-05 00:16:23 +0800

    finish getRsEventBetween

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -10,13 +10,17 @@ import java.util.List;
 @RestController
 public class RsController {
   private static List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
-  @GetMapping("/rs/list")
+ /* @GetMapping("/rs/list")
   public String getList(){
     return rsList.toString();
-  }
+  }*/
   @GetMapping("/rs/{index}")
   public String getOneRsEvent(@PathVariable int index){
     return rsList.get(index - 1);
+  }
+  @GetMapping("/rs/list")
+  public String getRsEventBetween(int start, int end){
+    return rsList.subList(start-1,end).toString();
   }
 }
 
--- src/test/java/com/thoughtworks/rslist/RsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/RsControllerTest.java
@@ -36,4 +36,18 @@ public class RsControllerTest {
                 .andExpect(status().isOk());
 
     }
+
+    @Test
+    void shouldGetRsEventBetween() throws Exception {
+        mockMvc.perform(get("/rs/list?start=1&end=2"))
+                .andExpect(content().string("[第一条事件, 第二条事件]"))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list?start=2&end=3"))
+                .andExpect(content().string("[第二条事件, 第三条事件]"))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list?start=1&end=3"))
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"))
+                .andExpect(status().isOk());
+
+    }
 }
