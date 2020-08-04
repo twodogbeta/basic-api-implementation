ecad5270fd4694a3e4e13eaea87f067fce701e78
twodogbeta <qijinhaoup@163.com>
2020-08-04 23:09:55 +0800

    finish routing exercise

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,21 @@
 package com.thoughtworks.rslist.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+
 public class RsController {
   private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  @GetMapping("/rs/list")
+  public String getList(){
+    return rsList.toString();
+  }
+  public void addOneRsEvent(@RequestBody String rsEvent){
+    rsList.add(rsEvent);
+  }
+
 }
