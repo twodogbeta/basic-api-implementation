c83f6d319a068d42e2c4690f04e3902ba7fa307c
twodogbeta <qijinhaoup@163.com>
2020-08-05 01:22:22 +0800

    Creat package domain and creat RsEvent

---
+++ src/main/java/com/thoughtworks/rslist/domain/RsEvent.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.rslist.domain;
+
+public class RsEvent<eventName> {
+    private String eventName;
+    private String keyWord;
+
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getKeyWord() {
+        return keyWord;
+    }
+
+    public void setKeyWord(String keyWord) {
+        this.keyWord = keyWord;
+    }
+}
