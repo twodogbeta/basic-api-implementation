b21421268b9d2b2e7c0c279d739e871d79259255
twodogbeta <qijinhaoup@163.com>
2020-08-08 16:20:02 +0800

    [Jim] add: log in exceptionHandler,Log error level information

--- src/main/java/com/thoughtworks/rslist/component/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/rslist/component/GlobalExceptionHandler.java
@@ -1,25 +1,54 @@
 package com.thoughtworks.rslist.component;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.rslist.exception.CommonError;
 import com.thoughtworks.rslist.exception.InvalidIndexException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler({InvalidIndexException.class, MethodArgumentNotValidException.class})
-    public ResponseEntity exceptionHandler (Exception ex){
+    public ResponseEntity exceptionHandler (Exception ex) throws JsonProcessingException {
         String errorMessage ;
         CommonError commonError = new CommonError();
         if (ex instanceof  MethodArgumentNotValidException)
             errorMessage  = "invalid param";
         else  errorMessage = ex.getMessage();
         commonError.setError(errorMessage);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String errorJson = objectMapper.writeValueAsString(commonError);
+        logger.error("测试错误日志输出：" + errorJson);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(commonError);
     }
 }
+
+
+/*@RestController
+ class LoggingController {
+
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+   // @RequestMapping("/")
+    public String index() {
+        logger.trace("这好像是一条TRACE信息");
+        logger.debug("这好像是一条DEBUG信息");
+        logger.info("这好像是一条INFO信息");
+        logger.warn("这好像是一条WARN信息 ");
+        logger.error("这好像是一条错误信息");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+}*/
+
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -2,7 +2,10 @@ package com.thoughtworks.rslist;
 
 import com.thoughtworks.rslist.domain.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,13 +13,18 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RsListApplicationTests {
-
+    @Autowired
+    MockMvc mockMvc;
   /*  @Test
     void shouldConnectDataBase() throws SQLException {
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/rsSystem", "root"  , "twodogs19") ;
         Statement stmt = connection.createStatement();
 
     }*/
+
+
+
 
 }
